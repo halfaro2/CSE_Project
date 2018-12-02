84701727cf3c1c09310aa9cc151136c0a9a2a0a1
Hector Alfaro <alfaro.hector@live.com>
2018-12-02 15:03:31 -0800

    Pushed icons

---
+++ background.h
@@ -0,0 +1,25 @@
+//
+//  background.h
+//  glutapp
+//
+//  Created by Hector Alfaro on 12/2/18.
+//  Copyright © 2018 Angelo Kyrilov. All rights reserved.
+//
+
+#ifndef background_h
+#define background_h
+
+class Background : public TextRect {
+private:
+    float x;
+    float y;
+    float w;
+    float h;
+   
+    
+public:
+    <#member functions#>
+};
+
+
+#endif /* background_h */
