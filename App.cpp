7d249937128e2f7a895c5cf91e08c79371921187
Justin Vargas <jvargas53@ucmerced.edu>
2018-12-13 13:32:02 -0800

    moved red and blue car to correct starting position

--- App.cpp
+++ App.cpp
@@ -15,24 +15,23 @@ App::App(int argc, char** argv): GlutApp(argc, argv){
     // sixth value is the width
     // seventh value is the height
     
-    //background
+  //background
     Background = new TexRect("CityBackground.bmp",-1, 1, 2, 2);
 
     //the taxi (player)
     TaxiFront = new AnimatedRect("Taxi-Back-Yellow-icon.bmp", 1, 1, 100, .1, -0.6, 0.5, 0.5);    //TaxiFront
     //-0.6 left
-    //.1 right
+    //.1
     
-    //right lane
-    RedCar.push_back(new AnimatedRect("blue-tesla.png", 1, 1, 10, 0.045, 0.4, 0.1, 0.1));
-    //middle lane
-    RedCar.push_back(new AnimatedRect("Car-Front-Red-icon.png", 1, 1, 10, -0.045, 0.4, 0.1, 0.1));
     //left lane
-    RedCar.push_back(new AnimatedRect("blue-tesla.png", 1, 1, 10, -0.14, 0.4, 0.1, 0.1));
+    RedCar.push_back(new AnimatedRect("Car-Front-Red-icon.png", 1, 1, 10, -0.1, 0.04, 0.12, 0.12));
+
+    //right lane
+    RedCar.push_back(new AnimatedRect("blue-tesla.png", 1, 1, 10, 0.01, 0.03, 0.1, 0.1));
     
     //added coin for an extra 10 points  
     //need to make them come later once the timer starts to not conflict with cars coming  
-    Coin.push_back(new AnimatedRect("bitcoin-icon.bmp", 1, 1, 100, -.090, 0, .15, .15));
+    Coin.push_back(new AnimatedRect("bitcoin-icon.bmp", 1, 1, 100, -.090, .5, .15, .15));
     
    
   
